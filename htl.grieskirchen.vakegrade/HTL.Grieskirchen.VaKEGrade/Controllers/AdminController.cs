fcd5c61cd21398e572476af82d88f17a150c79f9
dave.alex.maier <dave.alex.maier@gmail.com>
2011-11-07 12:29:20 +0000

    design uploadconfig

--- htl.grieskirchen.vakegrade/HTL.Grieskirchen.VaKEGrade/Controllers/AdminController.cs
+++ htl.grieskirchen.vakegrade/HTL.Grieskirchen.VaKEGrade/Controllers/AdminController.cs
@@ -14,18 +14,69 @@ namespace HTL.Grieskirchen.VaKEGrade.Controllers
 
         public ActionResult Index()
         {
-            if (IsAuthorized()) {
-
+            if (IsAuthorized())
+            {
                 return View();
+                
             }
             ViewData["error"] = "Bitte melden sie sich am System an";
             return Redirect("/Home/");
         }
 
-        public bool IsAuthorized() {
-            return Session["User"] != null && Session["Role"].ToString() == "Admin";
+        [HttpPost]
+        public ActionResult RecieveStudentConfig() 
+        {
+            if (IsAuthorized())
+            {
+                foreach (string inputTagName in Request.Files)
+                {
+                    HttpPostedFileBase file = Request.Files[inputTagName];
+                    if (file.ContentLength > 0)
+                    {
+
+                        byte[] config = new byte[file.ContentLength];
+                        file.InputStream.Read(config, 0, file.ContentLength);
+
+
+                        String s = System.Text.UnicodeEncoding.UTF8.GetString(config);
+                        string[] data = s.Split(Environment.NewLine.ToCharArray());
+                    }
+                }
+                return RedirectToAction("Index");
+                
+            }
+            ViewData["error"] = "Bitte melden sie sich am System an";
+            return Redirect("/Home/");
+            
+            
+            
+
+            
         }
 
-        
+
+            
+
+        public bool IsAuthorized() {
+            return Session["User"] != null && Session["Role"].ToString() == "Admin";
+
+        }
+
+
+        public JsonResult RetrieveAllStudents()
+        {
+            if (IsAuthorized())
+            {
+            JsonResult jres = Json(new { }, JsonRequestBehavior.AllowGet);
+
+            return jres;
+            }
+            ViewData["error"] = "Bitte melden sie sich am System an";
+            return null;
+        }
+
+
     }
+
+    
 }
