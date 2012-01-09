7c1d8aae35119df27f34a981df9b2df826dbf18a
dave.alex.maier <dave.alex.maier@gmail.com>
2012-01-09 07:40:54 +0000

    dsfdsf

---
+++ htl.grieskirchen.vakegrade/HTL.Grieskirchen.VaKEGrade/Utility/JQGridConf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trirand.Web.Mvc;
+
+
+namespace HTL.Grieskirchen.VaKEGrade.Utility
+{
+    public class JQGridConf 
+    {
+        List<Column> colModel = new List<Column>();
+
+        public List<Column> ColModel
+        {
+            get { return colModel; }
+            set { colModel = value; }
+        }
+        List<string> colNames = new List<string>();
+
+        public List<String> ColNames
+        {
+            get { return colNames; }
+            set { colNames = value; }
+        }
+
+    }
+}
