0002c5637194e2ebd0d5abd29deaa0912c5f8a30
dave.alex.maier@gmail.com <dave.alex.maier@gmail.com@c14edfa1-5dec-52b1-bb46-29c4567954f6>
2012-01-09 07:41:18 +0000

    dsfdsf

---
+++ trunk/htl.grieskirchen.vakegrade/HTL.Grieskirchen.VaKEGrade/Utility/Column.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTL.Grieskirchen.VaKEGrade.Utility
+{
+    public class Column
+    {
+
+
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        string index;
+
+        public string Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+        int width;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        string align;
+
+        public string Align
+        {
+            get { return align; }
+            set { align = value; }
+        }
+
+    }
+}
